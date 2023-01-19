473568c512c8e10f9343afc607f3490968107cab
YaszMoon <yasminite@gmail.com>
2023-01-19 23:03:54 +0000

    Update scores.js with addNewScore() and Clear Score event listener

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,3 +1,20 @@
-var highscoreList = document.getElementById("highscores");
-highscoreList.setAttribute("class", "scores");
+var highscoreList = document.querySelector("#highscores");
+var wrapperEl = document.querySelector(".wrapper");
+wrapperEl.setAttribute("class", "scores");
 
+function addNewScore() {
+  var newHighscore = document.createElement("li");
+  var userInitials = localStorage.getItem("userInitials");
+  var newScore = localStorage.getItem("newScore");
+  newHighscore.textContent = `${userInitials} - ${newScore}`;
+  highscoreList.append(newHighscore);
+}
+
+addNewScore()
+
+// Clear highscores
+var clearScores = document.querySelector('#clear')
+// Clear Scores Event Listener
+clearScores.addEventListener("click", function() {
+    highscoreList.innerHTML = ""
+})
