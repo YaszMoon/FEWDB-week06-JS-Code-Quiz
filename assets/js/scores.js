4e600e11da28dd7372e6df7a6eb91ae2887a86c7
YaszMoon <yasminite@gmail.com>
2023-01-19 22:16:09 +0000

    Reset scores.js. Initials and highscore now being stored in local storage

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,13 +1,17 @@
 // Submit to Highscore
 // On submit display textContent with initials and score in Highscore List
 
-var submitScore = document.getElementById("submit");
-var highscoreList = document.getElementById("highscores");
-var userInitials = document.getElementById("initials");
+var submitScore = document.querySelector("#submit");
+var userInitials = document.querySelector("#initials");
+// var highscoreList = document.getElementById("highscores");
+
+// var userInitials = document.getElementById("initials");
+// highscoreList.setAttribute("class", "scores");
+// window.location.assign("highscores.html");
 
 // Submit Score Event Listener
-submitScore.addEventListener("click", function (event) {
-  window.location.assign("highscores.html");
-  highscoreList.setAttribute("class", "scores");
+submitScore.addEventListener("click", function () {
   localStorage.setItem("userInitials", userInitials.value);
+  localStorage.setItem("newScore", score);
+//   
 });
