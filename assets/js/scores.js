a2329881924d60bf3948518f8b52082abb0a5d27
YaszMoon <yasminite@gmail.com>
2023-01-19 12:40:26 +0000

    Update scores.js also add same script to html

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,5 +1,13 @@
-var finalScore = 0
+var finalScore = document.getElementById("final-score");
+var runningScore = 0;
+finalScore.textContent = runningScore;
 // if right answer clicked score plus 1
-var userInitials = prompt("Enter your initials")
-// display textContent with initials and score
-// `${userInitials} - ${finalScore}`
+var userInitials = document.getElementById("initials");
+// On submit display textContent with initials and score
+var submitScore = document.getElementById("submit");
+var feedback = document.getElementById("feedback");
+submitScore.addEventListener("click", function (event) {
+  endScreen.setAttribute("class", "hide");
+  feedback.setAttribute("class", "feedback");
+  feedback.textContent = `${userInitials.value} - ${runningScore}`;
+});
